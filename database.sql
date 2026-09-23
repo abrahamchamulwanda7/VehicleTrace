a1967abd0567cdff61800610c83dd324c660d0eb
Abraham Chamulwanda <abrahamchamulwanda7@gmail.com>
2026-09-23 16:56:46 +0200

    Recreate database script

--- database.sql
+++ database.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS vehicletrace;
+
+CREATE DATABASE vehicletrace
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_uca1400_ai_ci;
+
 USE vehicletrace;
 
 CREATE TABLE garages (
@@ -56,7 +62,8 @@ CREATE TABLE parts_used (
     quantity   INT NOT NULL CHECK (quantity > 0),
     cost       DECIMAL(10,2) NOT NULL CHECK (cost >= 0),
     FOREIGN KEY (repair_id) REFERENCES repairs(repair_id) ON DELETE CASCADE
-) ENGINE=InnoDB;visitorlog_dbvehicletracevehicletracevehicletrace
+) ENGINE=InnoDB;
+
 CREATE USER IF NOT EXISTS 'vehicletrace_app'@'localhost' IDENTIFIED BY 'VtApp2026!';
 CREATE USER IF NOT EXISTS 'vehicletrace_app'@'127.0.0.1' IDENTIFIED BY 'VtApp2026!';
 GRANT SELECT, INSERT, UPDATE, DELETE ON vehicletrace.* TO 'vehicletrace_app'@'localhost';
